e90393c3bfa39cd5d1913381f9c42dad8ad1e31c
mminici <acermarco95@gmail.com>
2024-04-26 21:23:17 +0200

    1. added template for "run" scripts 2. readapted node2vec and nodepruning scripts to the new template 3. adapted data_loader and utils

--- src/run_NodePruning.py
+++ src/run_NodePruning.py
@@ -1,59 +1,49 @@
 import os
-import shutil
 import mlflow
-import networkx as nx
+import shutil
 import numpy as np
+import networkx as nx
 
-from tqdm import tqdm
-from my_utils import set_seed, setup_env
 from data_loader import create_data_loader
-from model_eval import get_best_threshold, eval_pred, TestLogMetrics
+from model_eval import TestLogMetrics, eval_pred, get_best_threshold
+from my_utils import set_seed, setup_env, move_data_to_device
+from tqdm import tqdm
+
+DEFAULT_HYPERPARAMETERS = {'train_perc': 0.7,
+                           'val_perc': 0.15,
+                           'test_perc': 0.15,
+                           'overwrite_data': False}
+DEFAULT_TRAIN_HYPERPARAMETERS = {'metric_to_optimize': 'f1_macro'}
+DEFAULT_MODEL_HYPERPARAMETERS = {}
 
 
 # noinspection PyShadowingNames
-def run_experiment(dataset_name='UAE_sample',
+def run_experiment(dataset_name='cuba',
+                   is_few_shot=False,
+                   num_splits=10,
+                   device_id="",
                    seed=0,
-                   train_few_shot_samples=10,
-                   val_few_shot_samples=10,
-                   test_perc=0.2,
-                   num_splits=5,
-                   metric_to_optimize='f1_macro',
-                   device_id='1',
-                   overwrite_data=False):
-    # Save parameters
-    mlflow.log_param('model', 'NodePruning')
+                   hyper_parameters=DEFAULT_HYPERPARAMETERS,
+                   train_hyperparameters=DEFAULT_TRAIN_HYPERPARAMETERS,
+                   model_hyper_parameters=DEFAULT_MODEL_HYPERPARAMETERS
+                   ):
+    # Start experiment
+    # save parameters
     mlflow.log_param('dataset_name', dataset_name)
-    mlflow.log_param('overwrite_data', overwrite_data)
-    mlflow.log_param('train_few_shot_samples', train_few_shot_samples)
-    mlflow.log_param('val_few_shot_samples', val_few_shot_samples)
-    mlflow.log_param('test_perc', test_perc)
-    mlflow.log_param('seed', seed)
-    mlflow.log_param('num_splits', num_splits)
-    mlflow.log_param('metric_to_optimize', metric_to_optimize)
 
     # set seed for reproducibility
     set_seed(seed)
     # set device
     os.environ['CUDA_VISIBLE_DEVICES'] = device_id
     device, base_dir, interim_data_dir, data_dir = setup_env(device_id, dataset_name, seed, num_splits,
-                                                             train_few_shot_samples, val_few_shot_samples,
-                                                              test_perc)
+                                                             is_few_shot,
+                                                             hyper_parameters)
     print(data_dir)
-    # Create data loader for datasets
-    datasets = create_data_loader(dataset_name, base_dir, data_dir,
-                                  hyper_params={'overwrite_data': overwrite_data,
-                                                'train_few_shot_samples': train_few_shot_samples,
-                                                'val_few_shot_samples': val_few_shot_samples,
-                                                'test_perc': test_perc,
-                                                'seed': seed,
-                                                'num_splits': num_splits,
-                                                },
-                                  device=device)
-    # Remap nodeIDs
-    node_ids_list = list(datasets['graph'].nodes())
-    # Relabel each node from 0 to N-1
-    node_remapping = {node_ids_list[i]: i for i in range(len(node_ids_list))}
-    datasets['graph'] = nx.relabel_nodes(datasets['graph'], node_remapping)
+    # Create data loader for signed datasets
+    datasets = create_data_loader(dataset_name, base_dir, data_dir, hyper_parameters)
+
+    # Transfer data to device
+    datasets = move_data_to_device(datasets, device)
 
     # Compute node centrality values
     # TODO: take the type of centrality as input parameter
@@ -73,16 +63,20 @@ def run_experiment(dataset_name='UAE_sample',
         predicted_labels[coordinated_users] = 0
         predicted_labels_list.append(np.copy(predicted_labels))
 
-    # Initialize metrics loggers
+    # Create loggers
     val_logger = TestLogMetrics(num_splits, ['accuracy', 'precision', 'f1_macro', 'f1_micro'])
     test_logger = TestLogMetrics(num_splits, ['accuracy', 'precision', 'f1_macro', 'f1_micro'])
 
-    for run_id in tqdm(range(num_splits), 'data split'):
+    for run_id in range(num_splits):
+        print(f'Split {run_id + 1}/{num_splits}')
+
         # Since this is an unsupervised baseline, we merge training and validation
         unsupervised_mask = np.logical_or(datasets['splits'][run_id]['train'], datasets['splits'][run_id]['val'])
         # Select the best threshold according to the eval on train+val sets
         best_val_threshold = get_best_threshold(datasets['labels'],
-                                                predicted_labels_list, unsupervised_mask, metric_to_optimize)
+                                                predicted_labels_list,
+                                                unsupervised_mask,
+                                                train_hyperparameters["metric_to_optimize"])
         val_metrics = eval_pred(datasets['labels'], predicted_labels_list[best_val_threshold], unsupervised_mask)
         # Compute test statistics
         test_metrics = eval_pred(datasets['labels'], predicted_labels_list[best_val_threshold],
@@ -91,15 +85,14 @@ def run_experiment(dataset_name='UAE_sample',
             test_logger.update(metric_name, run_id, test_metrics[metric_name])
             val_logger.update(metric_name, run_id, val_metrics[metric_name])
 
-    # Simulation ended, report metrics on test set for the best model
     print('Val set: ')
     for metric_name in val_logger.test_metrics_dict:
         avg_val, std_val = val_logger.get_metric_stats(metric_name)
-        mlflow.log_metric('val_' + metric_name + '_avg', avg_val)
-        mlflow.log_metric('val_' + metric_name + '_std', std_val)
+        mlflow.log_metric(metric_name + '_avg', avg_val)
+        mlflow.log_metric(metric_name + '_std', std_val)
         np.save(file=interim_data_dir / f'val_{metric_name}', arr=np.array(val_logger.test_metrics_dict[metric_name]))
         mlflow.log_artifact(interim_data_dir / f'val_{metric_name}.npy')
-        print(f'Val {metric_name}: {avg_val}+-{std_val}')
+        print(f'[VAL] {metric_name}: {avg_val}+-{std_val}')
 
     print('Test set: ')
     for metric_name in test_logger.test_metrics_dict:
@@ -108,36 +101,39 @@ def run_experiment(dataset_name='UAE_sample',
         mlflow.log_metric(metric_name + '_std', std_val)
         np.save(file=interim_data_dir / f'{metric_name}', arr=np.array(test_logger.test_metrics_dict[metric_name]))
         mlflow.log_artifact(interim_data_dir / f'{metric_name}.npy')
-        print(f'Test {metric_name}: {avg_val}+-{std_val}')
+        print(f'[TEST] {metric_name}: {avg_val}+-{std_val}')
 
 
 if __name__ == '__main__':
     # Run input parameters
     dataset_name = 'cuba'
-    train_few_shot_samples = [3, 5, 10, 20]
-    test_perc = 0.2
-    # val_few_shot_samples = [5, 10, 25, 50, 100, 250, 500, 1000]
+    train_perc = 0.70
+    val_perc = 0.15
+    test_perc = 0.15
+    overwrite_data = False
+    is_few_shot = False
     seed = [0, ]
-    num_splits = [20, ]
-    device_id = '1'
-    metric_to_optimize = 'f1_macro'
+    num_splits = [10, ]
+    # General hyperparameters
+    hyper_parameters = {'train_perc': train_perc, 'val_perc': val_perc, 'test_perc': test_perc,
+                        'overwrite_data': overwrite_data, 'traces_list': ['coRT']}
+    # optimization hyperparameters
+    train_hyper_parameters = {'metric_to_optimize': 'f1_macro'}
+    # model hyperparameters
+    model_hyper_parameters = {}
     for seed_val in seed:
         mlflow.set_experiment(f'{dataset_name}-NodePruning-{seed_val}')
-        for train_few_shot_samples_val in train_few_shot_samples:
-            val_few_shot_samples_val = train_few_shot_samples_val
-            for num_splits_val in num_splits:
-                with mlflow.start_run():
-                    exp_dir = run_experiment(dataset_name=dataset_name,
-                                             seed=seed_val,
-                                             train_few_shot_samples=train_few_shot_samples_val,
-                                             val_few_shot_samples=val_few_shot_samples_val,
-                                             test_perc=test_perc,
-                                             num_splits=num_splits_val,
-                                             metric_to_optimize=metric_to_optimize,
-                                             device_id=device_id,
-                                             overwrite_data=False
-                                             )
-                    try:
-                        shutil.rmtree(exp_dir, ignore_errors=True)
-                    except OSError as e:
-                        print("Error: %s - %s." % (e.filename, e.strerror))
+        for num_splits_val in num_splits:
+            with mlflow.start_run():
+                exp_dir = run_experiment(dataset_name=dataset_name,
+                                         is_few_shot=is_few_shot,
+                                         num_splits=num_splits_val,
+                                         seed=seed_val,
+                                         hyper_parameters=hyper_parameters,
+                                         train_hyperparameters=train_hyper_parameters,
+                                         model_hyper_parameters=model_hyper_parameters
+                                         )
+                try:
+                    shutil.rmtree(exp_dir, ignore_errors=True)
+                except OSError as e:
+                    print("Error: %s - %s." % (e.filename, e.strerror))
--- src/run_Template.py
+++ src/run_Template.py
@@ -5,85 +5,49 @@ import shutil
 import numpy as np
 
 from data_loader import create_data_loader
-from data_processor import get_edge_index_with_sign
-from model_eval import eval_model, TrainLogMetrics, TestLogMetrics
+from model_eval import TrainLogMetrics, TestLogMetrics
 from my_utils import set_seed, setup_env, move_data_to_device
-from models import MySDGNN
 from plot_utils import plot_losses
 
 
+DEFAULT_HYPERPARAMETERS = {'train_perc': 0.7,
+                           'val_perc': 0.15,
+                           'test_perc': 0.15,
+                           'overwrite_data': False}
+DEFAULT_TRAIN_HYPERPARAMETERS = {'num_epochs': 100,
+                                 'learning_rate': 1e-3,
+                                 'early_stopping_limit': 10,
+                                 'metric_to_optimize': 'auc_score',
+                                  }
+DEFAULT_MODEL_HYPERPARAMETERS = {'latent_dim': 32}
+
 # noinspection PyShadowingNames
-def run_experiment(dataset_name='wiki',
-                   overwrite_data=False,
-                   learnable_features=False,
-                   is_transductive=False,
-                   random_masking=True,
-                   unlabeled_perc=None,
-                   val_perc=0.1,
-                   test_perc=0.1,
-                   mask_perc=0.05,
-                   noise_perc=0.0,
-                   seed=0,
+def run_experiment(dataset_name='cuba',
+                   is_few_shot=False,
                    num_splits=10,
-                   device_id='',
-                   learning_rate=0.001,
-                   weight_decay=0.001,
-                   num_epochs=1000,
-                   check_loss_freq=25,
-                   latent_dim=4,
-                   num_layers=2,
-                   metric_to_optimize='auc_score',
-                   early_stopping_limit=10):
+                   device_id="",
+                   seed=0,
+                   hyper_parameters=DEFAULT_HYPERPARAMETERS,
+                   train_hyperparameters=DEFAULT_TRAIN_HYPERPARAMETERS,
+                   model_hyper_parameters=DEFAULT_MODEL_HYPERPARAMETERS
+                   ):
     # Start experiment
     # save parameters
     mlflow.log_param('dataset_name', dataset_name)
-    mlflow.log_param('overwrite_data', overwrite_data)
-    mlflow.log_param('learnable_features', learnable_features)
-    mlflow.log_param('random_masking', random_masking)
-    mlflow.log_param('val_perc', val_perc)
-    mlflow.log_param('test_perc', test_perc)
-    mlflow.log_param('noise_perc', noise_perc)
-    mlflow.log_param('mask_perc', mask_perc)
-    mlflow.log_param('unlabeled_perc', unlabeled_perc)
-    mlflow.log_param('seed', seed)
-    mlflow.log_param('num_splits', num_splits)
-    mlflow.log_param('learning_rate', learning_rate)
-    mlflow.log_param('weight_decay', weight_decay)
-    mlflow.log_param('num_epochs', num_epochs)
-    mlflow.log_param('latent_dim', latent_dim)
-    mlflow.log_param('num_layers', num_layers)
-    mlflow.log_param('metric_to_optimize', metric_to_optimize)
-    mlflow.log_param('early_stopping_limit', early_stopping_limit)
 
     # set seed for reproducibility
     set_seed(seed)
     # set device
     os.environ['CUDA_VISIBLE_DEVICES'] = device_id
-    device, base_dir, interim_data_dir, data_dir = setup_env(device_id, dataset_name, seed, num_splits, val_perc,
-                                                             test_perc, mask_perc, noise_perc,
-                                                             filter_transitive=False,
-                                                             is_transductive=is_transductive,
-                                                             random_masking=random_masking)
+    device, base_dir, interim_data_dir, data_dir = setup_env(device_id, dataset_name, seed, num_splits,
+                                                             is_few_shot,
+                                                             hyper_parameters)
     print(data_dir)
     # Create data loader for signed datasets
-    signed_datasets = create_data_loader(dataset_name, base_dir, data_dir,
-                                         hyper_params={'overwrite_data': overwrite_data,
-                                                       'is_transductive': is_transductive,
-                                                       'random_masking': random_masking,
-                                                       'unlabeled_perc': unlabeled_perc,
-                                                       'val_perc': val_perc,
-                                                       'test_perc': test_perc,
-                                                       'mask_perc': mask_perc,
-                                                       'noise_perc': noise_perc,
-                                                       'seed': seed,
-                                                       'num_splits': num_splits,
-                                                       'latent_dim': latent_dim,
-                                                       'filter_transitive': False,
-                                                       },
-                                         device=device)
+    datasets = create_data_loader(dataset_name, base_dir, data_dir, hyper_parameters)
 
     # Transfer data to device
-    signed_datasets = move_data_to_device(signed_datasets, device)
+    datasets = move_data_to_device(datasets, device)
 
     # Create loggers
     train_logger = TrainLogMetrics(num_splits, ['supervised'])
@@ -92,34 +56,21 @@ def run_experiment(dataset_name='wiki',
 
     for run_id in range(num_splits):
         print(f'Split {run_id + 1}/{num_splits}')
-        # Move validation and test labels to numpy for evaluation
-        for split_type in ['val', 'test']:
-            signed_datasets[run_id][split_type]['label'] = signed_datasets[run_id][split_type]['label'].detach().cpu().numpy()
 
-        # reformat edge index to have for each edge an additional entry equal to the sign
-        signed_datasets[run_id]['train']['label'] = signed_datasets[run_id]['train']['label'].float()
-        edge_index_with_sign = get_edge_index_with_sign(signed_datasets[run_id]['train']['edges'],
-                                                        signed_datasets[run_id]['train']['label']).long().to(device)
         # Create the model
-        model = MySDGNN(node_num=signed_datasets[run_id]['features'].shape[0],
-                        edge_index_s=edge_index_with_sign,
-                        learnable_features=learnable_features,
-                        in_dim=signed_datasets[run_id]['features'].shape[1],
-                        out_dim=latent_dim,
-                        layer_num=num_layers,
-                        lamb_d=1.0,
-                        lamb_t=0.0,
-                        features=signed_datasets[run_id]['features'].to(device))
+        model = create_model(model_hyper_parameters)
         model.to(device)
         optimizer = torch.optim.Adam(model.parameters(),
-                                     lr=learning_rate,
-                                     weight_decay=weight_decay)
+                                     lr=train_hyperparameters["learning_rate"],
+                                     weight_decay=train_hyperparameters["weight_decay"] if "weight_decay" in train_hyperparameters else None)
         BEST_VAL_METRIC = -np.inf
         best_model_path = interim_data_dir / f'model{run_id}.pth'
 
         early_stopping_cnt = 0
-        for epoch in range(num_epochs):
-            if early_stopping_cnt > early_stopping_limit:
+        num_epochs = train_hyperparameters['num_epochs']
+        metric_to_optimize = train_hyperparameters['metric_to_optimize']
+        for epoch in range(train_hyperparameters["num_epochs"]):
+            if early_stopping_cnt > train_hyperparameters["early_stopping_limit"]:
                 break
             model.train()
             optimizer.zero_grad()
@@ -128,18 +79,18 @@ def run_experiment(dataset_name='wiki',
             loss.backward()
             optimizer.step()
             train_logger.train_update(run_id, 'supervised', loss.item())
-            if epoch % check_loss_freq == 0:
+            if epoch % train_hyperparameters["check_loss_freq"] == 0:
                 # Validation step
                 model.eval()
                 with torch.no_grad():
                     val_metrics = eval_model(model,
-                                             signed_datasets[run_id]['train']['edges'],
-                                             signed_datasets[run_id]['val']['edges'],
-                                             signed_datasets[run_id]['train']['label'],
-                                             signed_datasets[run_id]['val']['label'])
-                    train_logger.val_update(run_id, val_metrics[metric_to_optimize])
-                    if val_metrics[metric_to_optimize] > BEST_VAL_METRIC:
-                        BEST_VAL_METRIC = val_metrics[metric_to_optimize]
+                                             datasets[run_id]['train']['edges'],
+                                             datasets[run_id]['val']['edges'],
+                                             datasets[run_id]['train']['label'],
+                                             datasets[run_id]['val']['label'])
+                    train_logger.val_update(run_id, val_metrics[train_hyperparameters["metric_to_optimize"]])
+                    if val_metrics[train_hyperparameters["metric_to_optimize"]] > BEST_VAL_METRIC:
+                        BEST_VAL_METRIC = val_metrics[train_hyperparameters["metric_to_optimize"]]
                         torch.save(model.state_dict(), best_model_path)
                         early_stopping_cnt = 0
                     else:
@@ -154,15 +105,15 @@ def run_experiment(dataset_name='wiki',
         model.eval()
         with torch.no_grad():
             test_metrics = eval_model(model,
-                                      signed_datasets[run_id]['train']['edges'],
-                                      signed_datasets[run_id]['test']['edges'],
-                                      signed_datasets[run_id]['train']['label'],
-                                      signed_datasets[run_id]['test']['label'])
+                                      datasets[run_id]['train']['edges'],
+                                      datasets[run_id]['test']['edges'],
+                                      datasets[run_id]['train']['label'],
+                                      datasets[run_id]['test']['label'])
             val_metrics = eval_model(model,
-                                      signed_datasets[run_id]['train']['edges'],
-                                      signed_datasets[run_id]['val']['edges'],
-                                      signed_datasets[run_id]['train']['label'],
-                                      signed_datasets[run_id]['val']['label'])
+                                      datasets[run_id]['train']['edges'],
+                                      datasets[run_id]['val']['edges'],
+                                      datasets[run_id]['train']['label'],
+                                      datasets[run_id]['val']['label'])
         for metric_name in test_metrics:
             test_logger.update(metric_name, run_id, test_metrics[metric_name])
             val_logger.update(metric_name, run_id, val_metrics[metric_name])
@@ -201,56 +152,47 @@ def run_experiment(dataset_name='wiki',
 
 if __name__ == '__main__':
     # Run input parameters
-    dataset_name = 'slashdot'
-    val_perc = 0.05
-    test_perc = 0.05
+    dataset_name = 'cuba'
+    train_perc = 0.70
+    val_perc = 0.15
+    test_perc = 0.15
     overwrite_data = False
-    learnable_features = False
-    is_transductive = False
-    random_masking = True
-    mask_perc = [0.75, ]
-    noise_perc = [0.0, 0.1, 0.2]
+    is_few_shot = False
     seed = [0, ]
-    num_splits = [20, ]
+    num_splits = [10, ]
     device_id = '3'
+    # General hyperparameters
+    hyper_parameters = {'train_perc': train_perc, 'val_perc': val_perc, 'test_perc': test_perc,
+                        'overwrite_data': overwrite_data}
     # optimization hyperparameters
     learning_rate = 0.001
-    weight_decay = 0.001
     num_epochs = 1000
     check_loss_freq = 25
     early_stopping_limit = 10
+    metric_to_optimize = 'auc_score'
+    train_hyper_parameters = {'num_epochs': num_epochs,
+                              'learning_rate': 1e-3,
+                              'early_stopping_limit': early_stopping_limit,
+                              'metric_to_optimize': metric_to_optimize,
+                              'check_loss_freq': check_loss_freq,
+                              }
     # model hyperparameters
     latent_dim = 64
-    num_layers = 2
-    metric_to_optimize = 'accuracy'
+    model_hyper_parameters = {'latent_dim': latent_dim}
     for seed_val in seed:
-        mlflow.set_experiment(f'{metric_to_optimize}-{dataset_name}-SDGNN-{seed_val}')
-        for noise_perc_val in noise_perc:
-            for mask_perc_val in mask_perc:
-                for num_splits_val in num_splits:
-                    with mlflow.start_run():
-                        exp_dir = run_experiment(dataset_name=dataset_name,
-                                                 overwrite_data=overwrite_data,
-                                                 learnable_features=learnable_features,
-                                                 is_transductive=is_transductive,
-                                                 random_masking=random_masking,
-                                                 noise_perc=noise_perc_val,
-                                                 val_perc=val_perc,
-                                                 test_perc=test_perc,
-                                                 mask_perc=mask_perc_val,
-                                                 seed=seed_val,
-                                                 num_splits=num_splits_val,
-                                                 device_id=device_id,
-                                                 learning_rate=learning_rate,
-                                                 weight_decay=weight_decay,
-                                                 num_epochs=num_epochs,
-                                                 check_loss_freq=check_loss_freq,
-                                                 latent_dim=latent_dim,
-                                                 num_layers=num_layers,
-                                                 metric_to_optimize=metric_to_optimize,
-                                                 early_stopping_limit=early_stopping_limit
-                                                 )
-                        try:
-                            shutil.rmtree(exp_dir, ignore_errors=True)
-                        except OSError as e:
-                            print("Error: %s - %s." % (e.filename, e.strerror))
+        mlflow.set_experiment(f'{metric_to_optimize}-{dataset_name}-MODEL-{seed_val}')
+        for num_splits_val in num_splits:
+            with mlflow.start_run():
+                exp_dir = run_experiment(dataset_name=dataset_name,
+                                         is_few_shot=is_few_shot,
+                                         num_splits=num_splits_val,
+                                         device_id=device_id,
+                                         seed=seed_val,
+                                         hyper_parameters=hyper_parameters,
+                                         train_hyperparameters=train_hyper_parameters,
+                                         model_hyper_parameters=model_hyper_parameters
+                                         )
+                try:
+                    shutil.rmtree(exp_dir, ignore_errors=True)
+                except OSError as e:
+                    print("Error: %s - %s." % (e.filename, e.strerror))
