62ca800e933e0611b83883d1776312acf3082647
Marco Minici <acermarco95@gmail.com>
2024-08-21 16:16:34 -0700

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,10 +1,6 @@
 import pickle
 
 DATASET_FILENAME = 'datasets.pkl'
-# Script constants
-CONTROL_FILE_IDX, IO_FILE_IDX = 0, 1
-filename_dict = {'UAE_sample': ['control_driver_tweets_uae_082019.jsonl', 'uae_082019_tweets_csv_unhashed.csv'],
-                 'cuba': ['control_driver_tweets_cuba_082020.jsonl', 'cuba_082020_tweets_csv_unhashed.csv']}
 
 
 def load_dataset(data_dir, filter_th, tr_perc, undersampling):
